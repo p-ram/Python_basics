5e24e69b6ecc9160403d4b8c1c731263ed2e00c3
p-ram <94652181+p-ram@users.noreply.github.com>
2021-11-19 20:31:16 -0800

    Add files via upload

---
+++ New Text Document.py
@@ -0,0 +1,33 @@
+print "Hello World, I am practicing basics of Python Array Datastructure!"
+"""
+arr=[5]
+print('single element',arr)
+
+arr.append(6)
+print(arr)
+
+a=len(arr)
+print(a)
+
+b=arr[1]
+print(b)
+
+print("iterate array")
+for x in arr:
+    print(x)
+    
+arr.append("asd")
+print(arr)
+
+for x in arr:
+    print(x)
+"""
+#remove element in arr
+arr=[1,2,3,4,'asd','asdasd']
+for x in arr:
+    arr.remove(x)
+    print(x)
+    print(type(x))
+
+    
+print(arr)
